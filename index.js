f98a1ec1ebfff8a41122afe80f7cf57803f0f6d4
Paige Miles <paige@lucas-miles.com>
2020-10-12 19:49:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const thankYouMessage = []
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+    thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouMessage
+}
+
+writeCards(names, event)
+
+function countDown(number){
+  while (number >= 0){
+    console.log(number--)
+  }
+}
+
+countDown(number)
