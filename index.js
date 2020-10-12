9d294ba7419f73ca5aaf5c6db16e4a6c41fbcc80
Paige Miles <paige@lucas-miles.com>
2020-10-12 19:28:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const thankYouMessage = []
+
+function writeCards(names, event){
+  for (let i = 0; i < names.length; i++){
+    thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouMessage
+}
+
+writeCards(names, event)
+
